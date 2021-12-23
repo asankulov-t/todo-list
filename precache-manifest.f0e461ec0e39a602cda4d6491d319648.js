109c9321da1ebb6cedd4575f73bee3808dff00ca
asankulov-t <asankulov-t>
2021-12-24 01:19:49 +0600

    Updates

---
+++ precache-manifest.f0e461ec0e39a602cda4d6491d319648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06ae9832ca648cef94ecf5a790e78f4",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "6c46af3b00a6dbd0e59a",
+    "url": "/todo-list/static/css/main.e1e6eb19.chunk.css"
+  },
+  {
+    "revision": "620439d1fe5c8a3a43a6",
+    "url": "/todo-list/static/js/2.9899dba6.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todo-list/static/js/2.9899dba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c46af3b00a6dbd0e59a",
+    "url": "/todo-list/static/js/main.a14affa2.chunk.js"
+  },
+  {
+    "revision": "f5ee74d33c5afdf3bdbc",
+    "url": "/todo-list/static/js/runtime-main.bd857350.js"
+  }
+]);
